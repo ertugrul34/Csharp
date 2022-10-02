9206b0b7d127163c1addd8c7ab9c554225d3b625
ertugrul34 <ertugrul_erol@hotmail.com>
2022-10-03 01:52:40 +0300

    First Commit

---
+++ KampIntro/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //type safety - tip güvenliği
+            // Do not repeat yourself - kendini tekrarlama
+
+            string kategoriEtiketi = "Kategori";
+            int ogrenciSayisi = 32000;
+            double faizOranı = 1.45;
+            bool sistemeGirisYapmisMi = true;
+            double dolarDun = 7.35;
+            double dolarBugun = 7.35;
+
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azazlış Görseli");
+            }
+            else if (dolarDun<dolarBugun)
+            {
+                Console.WriteLine("Artış Görseli");
+            }
+            else
+            {
+                Console.WriteLine("Sabit Görseli");
+            }
+
+
+            if (sistemeGirisYapmisMi == true)
+            {
+                Console.WriteLine("Ayarlar Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş Yap Butonu");
+            }
+                
+
+            Console.WriteLine(kategoriEtiketi);
+
+
+        }
+    }
+}
