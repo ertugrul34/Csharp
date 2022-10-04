2e7ac6ac165457247d5de673dace185fd37ae774
Ertuğrul EROL <73794808+ertugrul34@users.noreply.github.com>
2022-10-04 22:12:16 +0300

    Second

---
+++ Donguler/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] kurslar = new string[] { "Yazılım Geliştirici Yetiştirme Kampı", "Programlamaya başlangıç için temel kurs","Java","C++" };
+
+
+            for (int i = 0; i <kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+
+
+            //dizilerde döngü kurarken foreach kullanılması işi kolaylaştırır. yazımı kolaydır.
+
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+
+
+
+            Console.WriteLine("Sayfa Sonu");        
+        }
+    }
+}
