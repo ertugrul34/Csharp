28e0be7cba3e326297bf1996ff7bf75bf0a6c82c
Ertuğrul EROL <73794808+ertugrul34@users.noreply.github.com>
2022-10-05 15:24:21 +0300

    First Commit

---
+++ Conditionals/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Conditionals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = 20;
+
+            if (number==10)
+            {
+                Console.WriteLine("Number is 10");
+            }
+            else if (number==20)
+            {
+                Console.WriteLine("Number is 20");
+            }
+            else
+            {
+                Console.WriteLine("Number is not 10 or 20");
+            }
+
+            
+        }
+    }
+}
