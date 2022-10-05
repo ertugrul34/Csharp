b29bde43a0a84a6dc0e53afd202d1fd197a784f2
Ertuğrul EROL <73794808+ertugrul34@users.noreply.github.com>
2022-10-05 20:58:25 +0300

    First Commit

---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace OOP1
+{
+    public class ProductManager
+    {
+        public void Add(Product product)
+        {
+            Console.WriteLine("Ürün Eklendi :" +" "+ product.ProductName);
+        }
+    }
+}
