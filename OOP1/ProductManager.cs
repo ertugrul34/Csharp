6b39a60a0687cfc82e6d5d81d88f6802ac565c3b
Ertuğrul EROL <73794808+ertugrul34@users.noreply.github.com>
2022-11-03 21:02:27 +0300

    Second Commit

--- OOP1/ProductManager.cs
+++ OOP1/ProductManager.cs
@@ -3,9 +3,27 @@ namespace OOP1
 {
     public class ProductManager
     {
+        //encapsulation
         public void Add(Product product)
         {
             Console.WriteLine("Ürün Eklendi :" +" "+ product.ProductName);
+        }
+
+        public void Update(Product product)
+        {
+            Console.WriteLine("Ürün Güncellendi :" + " " + product.ProductName);
+        }
+
+        public int Topla(int sayi1, int sayi2)
+        {
+            return sayi1 + sayi2;
+
+        }
+
+        public void Topla2(int sayi1, int sayi2)
+        {
+            Console.WriteLine(sayi1 + sayi2);
+
         }
     }
 }
