bdd661e871162fd76b47af3e827d19686a437aa9
Ertuğrul EROL <73794808+ertugrul34@users.noreply.github.com>
2022-11-03 23:46:38 +0300

    Second Commit

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace GenericsIntro
+{
+    public class MyList<T>
+    {
+        T[] items;
+        //constructor
+        //class newlendiğinde items dizisinin newlenmesi için constructor içinde newlenmiştir.
+        public MyList()
+        {
+            items = new T[0];
+        }
+        public void Add(T item)
+        {
+            T[] tempArray = items; // newlendiğinde tutulan referanslar silineceğinden geçici dizi oluşturulurak ilk eleman saklanır
+            items = new T[items.Length + 1];
+
+            //temparray de geçici tutulan elemanların items dizisine eklenmesi for döngüsü ile yapıldı
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+
+            items[items.Length - 1] = item;
+
+        }
+    }
+}
