1a96a4a026d850a6b3d7c17e407e05f6bf9febec
Ertuğrul EROL <73794808+ertugrul34@users.noreply.github.com>
2022-11-04 16:53:28 +0300

    First Commit

---
+++ Constructors/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer() {Id=1, FirstName="Engin", LastName="Demiroğ", City="Ankara" };
+
+            Customer customer3 = new Customer();
+            customer3.Id = 3;
+
+            Customer customer2 = new Customer(2, "Derin", "Demiroğ", "Ankara");
+            Console.WriteLine(customer2.FirstName);
+
+        }
+    }
+
+    class Customer
+    {
+        //Hem parametreli hem de parametresiz ctor yaratıldı. İki tip kullanım için
+        public Customer()
+        {
+
+        }
+        //Constructor içi boş olsa bile default olarak vardır ve çalışır
+        public Customer(int id,string firstname,string lastname,string city)
+        {
+            this.Id = id;
+            this.FirstName = firstname;
+            this.LastName = lastname;
+            this.City = city;
+        }
+
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string City { get; set; }
+    }
+}
